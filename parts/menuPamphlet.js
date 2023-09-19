d51eaa4bb5a24a76ebb0203934b19386eb2f0ead
TyrkiskPeber <77719459+TyrkiskPeber@users.noreply.github.com>
2023-09-19 08:12:52 +0000

    Renamed and began construction of a better system

---
+++ parts/menuPamphlet.js
@@ -0,0 +1,13 @@
+function menuPamphlet() {
+    return (
+        `
+<div class="menu">
+
+    <h3 class="menuHead">Klassiska Pizzor</h3>
+    <div class="menuText">
+
+    </div>
+</div>
+`
+    );
+}
